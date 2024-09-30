e3688f2b064a3ce08fccd89585ebb98aee7dcd41
SsangSoo <kss10080@naver.com>
2024-10-01 01:59:59 +0900

    real section 01 end

---
+++ src/class1/ex3/ProductOrderMain.java
@@ -0,0 +1,31 @@
+package class1.ex3;
+
+public class ProductOrderMain {
+    public static void main(String[] args) {
+        ProductOrder dubu = new ProductOrder();
+        dubu.productName = "두부";
+        dubu.price = 2000;
+        dubu.quantity = 2;
+
+        ProductOrder kimchi = new ProductOrder();
+        kimchi.productName = "김치";
+        kimchi.price = 5000;
+        kimchi.quantity = 1;
+
+        ProductOrder cola = new ProductOrder();
+        cola.productName = "콜라";
+        cola.price = 1500;
+        cola.quantity = 2;
+
+        ProductOrder[] productOrders = {dubu, kimchi, cola};
+
+        int totalPrice = 0;
+        for(ProductOrder po : productOrders) {
+            System.out.println("상품명: " + po.productName + ", 가격: " + po.price + ", 수량: " + po.quantity);
+            totalPrice += po.price * po.quantity;
+        }
+        System.out.println("총 결제 금액: " + totalPrice);
+
+
+    }
+}
