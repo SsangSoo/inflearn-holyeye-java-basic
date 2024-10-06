06462f15c6a68758d9aa9c0af4a7e2393e70ca34
SsangSoo <kss10080@naver.com>
2024-10-07 06:13:00 +0900

    oop

--- src/oop1/ex/Rectangle.java
+++ src/oop1/ex/Rectangle.java
@@ -4,15 +4,15 @@ public class Rectangle {
     int width;
     int height;
 
-    void calculateArea() {
-        System.out.println("넓이 : " + width * height);
+    int calculateArea() {
+        return width * height;
     }
 
-    void calculatePerimeter() {
-        System.out.println("둘레 길이 : " + 2 * (width + height));
+    int calculatePerimeter() {
+        return 2 * (width + height);
     }
 
-    void isSquare() {
-        System.out.println("정사각형 여부: " + (width==height));
+    boolean isSquare() {
+        return width==height;
     }
 }
