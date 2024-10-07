fd515b051310d959d6d491e3f92d98b06f590521
SsangSoo <kss10080@naver.com>
2024-10-07 22:38:59 +0900

    constructor ~

---
+++ src/construct/MemberInit.java
@@ -0,0 +1,14 @@
+package construct;
+
+public class MemberInit {
+    String name;
+    int age;
+    int grade;
+
+    // 추가
+    void initMember(String name, int age, int grade) {
+        this.name = "user1";
+        this.age = 15;
+        this.grade = 90;
+    }
+}
