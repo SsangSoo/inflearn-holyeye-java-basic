6763889f6e816991938fbc7dd20a75bb1ea1505c
SsangSoo <kss10080@naver.com>
2024-10-30 01:02:05 +0900

    class and interface activity

---
+++ src/poly/ex6/SoundFlyMain.java
@@ -0,0 +1,26 @@
+package poly.ex6;
+
+
+public class SoundFlyMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        Chicken chicken = new Chicken();
+
+        soundAnimal(dog);
+        soundAnimal(bird);
+        soundAnimal(chicken);
+
+
+    }
+
+    //변하지 않는 부분
+    private static void soundAnimal(AbstractAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+
+}
