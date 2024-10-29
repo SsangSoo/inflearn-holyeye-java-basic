f34354f251e06fb3bdb3791bb6b0b82d6e820777
SsangSoo <kss10080@naver.com>
2024-10-29 23:51:19 +0900

    abstract ok

--- src/poly/ex3/AbstractMain.java
+++ src/poly/ex3/AbstractMain.java
@@ -1,24 +1,31 @@
-package poly.ex2;
+package poly.ex3;
 
-public class AnimalPolyMain1 {
+public class AbstractMain {
 
     public static void main(String[] args) {
+        //추상클래스 생성 불가
+        //AbstractAnimal animal = new AbstractAnimal();
+
         Dog dog = new Dog();
         Cat cat = new Cat();
         Caw caw = new Caw();
         Duck duck = new Duck();
 
-        soundAnimal(dog);
+        cat.sound();
+        cat.move();
+
         soundAnimal(cat);
+        soundAnimal(dog);
         soundAnimal(caw);
         soundAnimal(duck);
 
     }
 
-    private static void soundAnimal(Animal animal) {
+    private static void soundAnimal(AbstractAnimal animal) {
         System.out.println("동물 소리 테스트 시작");
         animal.sound();
         System.out.println("동물 소리 테스트 종료");
 
     }
+
 }
