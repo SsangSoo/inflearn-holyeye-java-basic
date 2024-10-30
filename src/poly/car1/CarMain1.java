f59461ed2d4dc382ae75b906102441c0d7b27c04
SsangSoo <kss10080@naver.com>
2024-10-31 00:49:09 +0900

    polymorphism ex ok

--- src/poly/car1/CarMain1.java
+++ src/poly/car1/CarMain1.java
@@ -11,9 +11,15 @@ public class CarMain1 {
         driver.setCar(k3Car);
         driver.drive();
 
-        //추가
+        //차량 변경(k3 -> model3)
         Model3Car model3Car = new Model3Car();
         driver.setCar(model3Car);
         driver.drive();
+
+        //차량 변경(model3 -> newCar)
+        NewCar newCar = new NewCar();
+        driver.setCar(newCar);
+        driver.drive();
+
     }
 }
