46a7fa56cd7aedcd06fa18f5d9b54a0d1be64488
SsangSoo <kss10080@naver.com>
2024-10-30 23:50:11 +0900

    java-basic-oop

--- src/poly/ex6/SoundFlyMain.java
+++ src/poly/ex6/SoundFlyMain.java
@@ -12,14 +12,25 @@ public class SoundFlyMain {
         soundAnimal(bird);
         soundAnimal(chicken);
 
+        flyAnimal(bird);
+        flyAnimal(chicken);
+
 
     }
 
-    //변하지 않는 부분
+    //AbstractAnimal 사용 가능
     private static void soundAnimal(AbstractAnimal animal) {
         System.out.println("동물 소리 테스트 시작");
         animal.sound();
         System.out.println("동물 소리 테스트 종료");
+    }
+
+    //Fly 인터페이스가 있으면 사용가능
+    private static void flyAnimal(Fly fly) {
+        System.out.println("날기 테스트 시작");
+        fly.fly();
+        System.out.println("날기 테스트 종료");
+
     }
 
 
